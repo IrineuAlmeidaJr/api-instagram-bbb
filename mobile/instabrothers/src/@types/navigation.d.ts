a584632df9684c9cb631eb5880e2aadeb2265f5e
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2023-01-30 11:15:12 -0300

    v0.1.7

--- mobile/instabrothers/src/@types/navigation.d.ts
+++
@@ -1,9 +0,0 @@
-export declare global {
-    namespace ReactNavigation {
-        interface RootParamList {
-            home: undefined;
-            allbrothersfollower: undefined;
-            detailsbrother: Brother;
-        }
-    }
-}
