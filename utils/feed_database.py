d1288cdcb53e941827311f38b62b152355a1c307
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2023-02-02 19:16:12 -0300

    v.0.1.12
    Updated dataset

--- utils/feed_database.py
+++ utils/feed_database.py
@@ -52,11 +52,11 @@ def main():
     pd.set_option('display.width', 1000)
 
     current_date = str(date.today())
-    brothers = LoadData.load_data_file()
-    # brothers = LoadData.load_data_intagram()
+    # brothers = LoadData.load_data_file()
+    brothers = LoadData.load_data_intagram()
     dataframe = create_dataframe(current_date, brothers)
     graphic_figure = create_graphic(brothers)
-    # write_documents(current_date, brothers, dataframe, graphic_figure)
+    write_documents(current_date, brothers, dataframe, graphic_figure)
 
 
 if __name__ == "__main__":
