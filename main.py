51ca3ef0e87fa1fa4f10096f1687b6d125b708fd
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2023-02-06 00:58:39 -0300

    Delete All

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-import os
-
-app = Flask(__name__)
-
-from routes.routes import *
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
