8e70442ab06e78adfc33679947984050f41b19a6
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2023-04-12 19:43:50 -0300

    Update database

--- model/StatusBrothers.py
+++ model/StatusBrothers.py
@@ -1,10 +1,10 @@
 class StatusBrothers:
 
     def __init__(self):
-        self.__leader = 9
+        self.__leader = 12
         self.__angel = 0
         self.__monster = []
-        self.__wall = [5, 7, 12]
+        self.__wall = [3, 4, 20]
         self.__in_game = [3, 4, 5, 8, 9, 10, 12, 13, 17, 20]
 
     @property
